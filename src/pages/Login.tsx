2bdb579505e52fdb0adfc1383965bd1610da84d0
sahilsoni20 <ssahil3120@gmail.com>
2024-06-05 23:42:23 +0530

    comit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,32 @@
 import { FcGoogle } from "react-icons/fc";
 import { signInWithPopup } from "firebase/auth";
 import { firebaseAuth, googleAuthProvider } from "../lib/FireBase";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
+    const navigate = useNavigate();
+
+    const signInWithGoogle = () => {
+        signInWithPopup(firebaseAuth, googleAuthProvider).then(() => {
+            navigate("/");
+        });
+    };
+
     return (
-        <>
-            
+        <>  
+            { firebaseAuth.currentUser ? (
+                ""
+            ) : (
+                <div>
+                    <h1>
+                        Want to Create a Post ?
+                        <span>Sign in First</span>
+                    </h1>
+                    <button onClick={signInWithGoogle}>
+                        <FcGoogle /> Sign in with Google
+                    </button>
+                </div>
+            )}
         </>
     );
 }
